c757c2021672ea0fb324e6e7a04b079aaaaf4e4c
David Nascimento <davidpn.11@gmail.com>
2018-09-15 13:04:11 -0300

    added clear method

--- src/routes/products.js
+++ src/routes/products.js
@@ -4,15 +4,13 @@ const mongoose = require('mongoose')
 const Product = require('../model/product')
 const multer = require('multer')
 const upload = multer({ dest: 'uploads/' })
+const { dd } = require('dumper.js')
 
 router.get('/', (req, res, next) => {
   Product.find()
     .exec()
     .then(docs => res.status(200).json(docs))
     .catch(err => res.status(500).json({ err }))
-  // res.status(200).json({
-  //   message: 'Orders were fetched',
-  // })
 })
 
 router.get('/:productId', (req, res, next) => {
@@ -38,6 +36,18 @@ router.delete('/:productId', (req, res, next) => {
   Product.remove({ _id })
     .exec()
     .then(doc => res.status(200).json(doc))
+    .catch(err => res.status(500).json({ err }))
+})
+
+router.post('/clear', (req, res) => {
+  Product.find()
+    .exec()
+    .then(docs => {
+      docs.map(doc => Product.deleteMany().exec())
+      // dd(docs)
+
+      return res.status(200).json({ status: 'database cleared' })
+    })
     .catch(err => res.status(500).json({ err }))
 })
 
