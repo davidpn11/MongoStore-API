5ee0543662512a4fd7e73d7846e1f5c53ed5bd1d
David Nascimento <davidpn.11@gmail.com>
2018-09-19 11:23:34 -0300

    fixed cloudname

--- src/routes/products.js
+++ src/routes/products.js
@@ -22,7 +22,7 @@ const fileFilter = (req, file, callback) => {
 }
 
 cloudinary.config({
-  cloud_name: 'cefet-mg',
+  cloud_name: 'davidpn11',
   api_key: process.env.CLOUDINARY_KEY,
   api_secret: process.env.CLOUDINARY_SECRET,
 })
